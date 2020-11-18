64c14770e80edd46a67144cc6745a48895dbea17
Lenard Mangay-ayam <lenard.mangayayam@gmail.com>
2020-11-18 09:15:59 +0800

    Add Config.js

--- public/js/config.js
+++ public/js/config.js
@@ -1,8 +1,7 @@
 window.config = {
-	"API":"http://localhost:8000/api/v1/", //URL WHERE YOUR API LOCATED
+	"API":"http://localhost:8000/api/v1/", //URL OF YOUR API LOCATED
 	"baseURL":"http://localhost:8000/", //URL OF YOUR WEBSITE
 	"storageURL":"http://localhost:8000/storage/", //URL WHERE YOUR IMAGES and OTHER FILEs stored
 	"debug":true
 }
 
-//http://localhost/laravel/public/git/voting-system/public/demo/admin/
