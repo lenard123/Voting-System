c08a29768cf76b2a6184967f8c4d670a40cd30de
Lenard Mangay-ayam <lenard.mangayayam@gmail.com>
2021-07-10 16:13:47 +0800

    Add All Components from Admin Routes

--- resources/assets/js/routesIndex.js
+++ resources/assets/js/routesIndex.js
@@ -8,10 +8,37 @@ import Result from './components/demo/voter/home/result.vue';
 import AdminLogin from './components/demo/admin/login.vue';
 import AdminIndex from './components/demo/admin/index.vue';
 
-import ManageElection from './components/demo/admin/home/home.vue';
-import ManageElectionIndex from './components/demo/admin/home/index.vue';
-import ManageElectionResult from './components/demo/admin/home/result.vue';
-import FinalResult from './components/demo/admin/home/final.vue';
+ import FinalResult from './components/demo/admin/home/final.vue';
+
+ import ManageElection from './components/demo/admin/home/home.vue';
+ import ManageElectionIndex from './components/demo/admin/home/index.vue';
+ import ManageElectionResult from './components/demo/admin/home/result.vue';
+
+ import ManagePosition from './components/demo/admin/position/position.vue';
+ import ManagePositionIndex from './components/demo/admin/position/index.vue';
+ import ManagePositionAdd from './components/demo/admin/position/add.vue';
+ import ManagePositionEdit from './components/demo/admin/position/edit.vue';
+
+ import ManagePartylist from './components/demo/admin/partylist/partylist.vue';
+ import ManagePartylistIndex from './components/demo/admin/partylist/index.vue';
+ import ManagePartylistAdd from './components/demo/admin/partylist/add.vue';
+ import ManagePartylistEdit from './components/demo/admin/partylist/edit.vue';
+
+ import ManageVoter from './components/demo/admin/voter/voter.vue';
+ import ManageVoterIndex from './components/demo/admin/voter/index.vue';
+ import ManageVoterAdd from './components/demo/admin/voter/add.vue';
+ import ManageVoterEdit from './components/demo/admin/voter/edit.vue';
+
+ import ManageNominee from './components/demo/admin/nominee/nominee.vue';
+ import ManageNomineeIndex from './components/demo/admin/nominee/index.vue';
+ import ManageNomineeAdd from './components/demo/admin/nominee/add.vue';
+ import ManageNomineeEdit from './components/demo/admin/nominee/edit.vue';
+
+ import ManageAccount from './components/demo/admin/admin/admin.vue';
+ import ManageAccountIndex from './components/demo/admin/admin/index.vue';
+ import ManageAccountEdit from './components/demo/admin/admin/edit.vue';
+ import ManageAccountPassword from './components/demo/admin/admin/password.vue';
+ import ManageAccountAdd from './components/demo/admin/admin/add.vue';
 
 const default_component = {
 	template: '<div>Not found: {{ $route.path }}</div>'
@@ -68,9 +95,10 @@ export default [
 			},
 
 			{
-				path: '',
-				component: ManageElection,
+				path: '', 
+				component: ManageElection, 
 				children: [
+
 					{
 						path: '',
 						component: ManageElectionIndex,
@@ -82,6 +110,142 @@ export default [
 						component: ManageElectionResult,
 						name: 'Result'
 					}
+
+				]
+			},
+
+			{
+				path: 'position',
+				component: ManagePosition,
+				children: [
+					
+					{
+						path:'',
+						component: ManagePositionIndex,
+						name: 'Manage Position'
+					},
+
+					{
+						path:'add',
+						component: ManagePositionAdd,
+						name: 'Add Position'
+					},
+
+					{
+						path: 'edit/:id',
+						component: ManagePositionEdit,
+						name: 'Edit Position'
+					}
+				]
+			},
+
+			{
+				path: 'partylist',
+				component: ManagePartylist,
+				children: [
+
+					{
+						path: '',
+						component: ManagePartylistIndex,
+						name: 'Manage Partylist'
+					},
+
+					{
+						path: 'add',
+						component: ManagePartylistAdd,
+						name: 'Add Partylist'
+					},
+
+					{
+						path: 'edit/:id',
+						component: ManagePartylistEdit,
+						name: 'Edit Partylist'
+					}
+
+				]
+			},
+
+			{
+				path: 'voter',
+				component: ManageVoter,
+				children: [
+
+					{
+						path: '',
+						component: ManageVoterIndex,
+						name: 'Manage Voter'
+					},
+
+					{
+						path: 'add',
+						component: ManageVoterAdd,
+						name: 'Add Voter'
+					},
+
+					{
+						path: 'edit/:id',
+						component: ManageVoterEdit,
+						name: 'Edit Voter'
+					}
+
+				]
+			},
+
+			{
+				path: 'nominee',
+				component: ManageNominee,
+				children: [
+
+					{
+						path: '',
+						component: ManageNomineeIndex,
+						name: 'Manage Nominee'
+					},
+
+					{
+						path: 'add',
+						component: ManageNomineeAdd,
+						name: 'Add Nominee'
+					},
+
+					{
+						path: 'edit/:id',
+						component: ManageNomineeEdit,
+						name: 'Edit Nominee'
+					}
+
+				]
+			},
+
+			{
+				path: '/account',
+				component: ManageAccount,
+				children: [
+
+					{
+						path: '',
+						component: ManageAccountIndex,
+						name: 'Manage Account'
+					},
+
+					{
+						path: 'update',
+						component: ManageAccountEdit,
+						name: 'Update Account'
+					},
+
+					{
+						path: 'password',
+						component: ManageAccountPassword,
+						name: 'Update Password'
+					},
+
+					{
+						path: 'add',
+						component: ManageAccountAdd,
+						name: 'Add Account'
+					}
+
 				]
 			}
 		]
